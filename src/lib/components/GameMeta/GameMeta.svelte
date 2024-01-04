2bfdfcca8e84e0860126dfe1b6635b39b92a4e6e
Robert Allen <robertcallen585@gmail.com>
2024-01-04 10:14:20 -0700

    Added screenshot + links section to games

---
+++ src/lib/components/GameMeta/GameMeta.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  export let googlePlayLink: string;
+  export let appleAppStoreLink: string;
+  export let privacyPolicyLink: string;
+
+  import googlePlayBadge from './google-play-app-badge.png';
+  import appleAppBadge from './apple-app-badge.png';
+</script>
+
+<div class="gameMeta">
+  <div class="badgeRow">
+    {#if googlePlayLink}
+      <a href={googlePlayLink} target="_blank">
+        <img src={googlePlayBadge} width="153px" height="45px" alt="Get It On Google Play" />
+      </a>
+    {/if}
+    {#if appleAppStoreLink}
+      <a href={appleAppStoreLink} target="_blank">
+        <img src={appleAppBadge} width="133px" height="45px" alt="Download on the App Store" />
+      </a>
+    {/if}
+  </div>
+  {#if privacyPolicyLink}
+    <a class="privacyPolicyLink" href={privacyPolicyLink} target="_blank">Privacy Policy</a>
+  {/if}
+</div>
+
+<style lang="scss">
+  .gameMeta {
+    background-color: #f3f3f3;
+    border-radius: 5px;
+    padding: 15px;
+    margin: 12px 0;
+    box-shadow: 0 5px 5px 0px rgba(0, 0, 0, 0.2);
+  }
+
+  .badgeRow {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    align-items: center;
+
+    img {
+      height: 45px;
+      margin-bottom: 8px;
+
+      &:first-of-type {
+        margin-right: 12px;
+      }
+    }
+  }
+
+  .privacyPolicyLink {
+    font-size: 1.4rem;
+  }
+</style>
--- src/routes/(main)/games/[game]/+page.svelte
+++ src/routes/(main)/games/[game]/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
   import { page } from '$app/stores';
   import SocialMediaHead from './SocialMediaHead.svelte';
+  import GameMeta from '$lib/components/GameMeta';
 
+  import image1 from './lock_breaker/phone_1.png';
+  import image2 from './lock_breaker/phone_2.png';
+  import image3 from './lock_breaker/phone_3.png';
+  import image4 from './lock_breaker/phone_4.png';
   export let data;
 </script>
 
@@ -10,14 +15,39 @@
 <div class="game container">
   <div class="row justify-content-center">
     <article class="col-md-8">
-      <img class="hero-image" alt="Lock Breaker" src={data.heroImageUrl} />
-      <div class="postMeta">
-        <h1 class="postTitle">{data.title}</h1>
-        <span class="postDate">Released: {data.date}</span>
-        <p class="lead">{data.lead}</p>
-        <div class="description">
-          <svelte:component this={data.component} />
+      <div class="postHead row">
+        <div class="imageWrap col-lg-4">
+          <img class="hero-image" alt="Lock Breaker" src={data.heroImageUrl} />
         </div>
+        <div class="col-lg-8">
+          <div class="postMeta">
+            <h1 class="postTitle">{data.title}</h1>
+            <p class="lead">{data.lead}</p>
+            <span class="postDate">Released: {data.date}</span>
+          </div>
+          <GameMeta
+            googlePlayLink="https://play.google.com/store/apps/details?id=net.rcallen.LockBreaker"
+            privacyPolicyLink="/privacy_policies/lock_breaker"
+          />
+        </div>
+      </div>
+      <h2>Screenshots</h2>
+      <div class="screenshots">
+        <div class="screenshot col-sm-3">
+          <img src={image1} alt="Lock Breaker Screenshot" />
+        </div>
+        <div class="screenshot col-sm-3">
+          <img src={image2} alt="Lock Breaker Screenshot" />
+        </div>
+        <div class="screenshot col-sm-3">
+          <img src={image3} alt="Lock Breaker Screenshot" />
+        </div>
+        <div class="screenshot col-sm-3">
+          <img src={image4} alt="Lock Breaker Screenshot" />
+        </div>
+      </div>
+      <div class="description">
+        <svelte:component this={data.component} />
       </div>
     </article>
   </div>
@@ -26,37 +56,54 @@
 <style lang="scss">
   .game {
     margin-top: 30px;
+
+    h2 {
+      font-size: 2rem;
+      font-weight: 600;
+      margin-top: 8px;
+      margin-bottom: 8px;
+    }
+  }
+
+  .imageWrap {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-right: 12px;
+    margin-bottom: 24px;
   }
 
   .hero-image {
     border-radius: 50px;
-    margin: 0 auto 40px;
+    max-width: 100%;
+    height: auto;
   }
 
   .postMeta {
     color: #888;
     line-height: 1.5;
-    margin-bottom: 2.4rem;
+    margin-bottom: 12px;
   }
 
   .postDate {
     display: block;
     font-size: 1.575rem;
     color: rgba(0, 0, 0, 0.5);
-    margin-bottom: 2rem;
   }
 
   .postTitle {
-    font-weight: 700;
+    font-weight: 600;
     margin-bottom: 0.8rem;
     font-size: 3.4rem;
-    color: rgb(41, 43, 44);
+    color: rgb(40, 40, 40);
     line-height: 1.2;
   }
 
   .lead {
-    font-size: 2rem;
+    font-size: 1.6rem;
     font-weight: 400;
+    color: #888;
+    margin-bottom: 24px;
   }
 
   .description :global(.markdown-body) {
@@ -65,6 +112,24 @@
     margin-top: 20px;
     padding-left: 0;
     padding-right: 0;
+    min-height: 15vh;
+  }
+
+  .screenshots {
+    display: flex;
+    max-width: 100%;
+    overflow-x: auto;
+    overflow-y: hidden;
+  }
+
+  .screenshot {
+    min-width: 200px;
+    padding-right: 24px;
+
+    img {
+      border-radius: 15px;
+      margin-bottom: 12px;
+    }
   }
 
   article {
