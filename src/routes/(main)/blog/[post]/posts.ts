5fe0d93607bad2efaf42a35ee4b3bbe5e035119c
Robert Allen <rob@binarysunrise.dev>
2024-03-16 07:25:11 +0900

    Move dev log post to better url

--- src/routes/(main)/blog/[post]/posts.ts
+++ src/routes/(main)/blog/[post]/posts.ts
@@ -2,11 +2,11 @@
 // @ts-nocheck
 export const posts = [
   {
-    href: '/blog/devlog_1',
+    href: '/blog/ori_like_platformer',
     title: 'Ori-like platformer prototype: Dev Log Mar 3rd - 9th',
     author: 'Robert Allen',
     date: '03-11-2024',
-    heroImageUrl: import('./devlog_1/bash_in_action.png'),
+    heroImageUrl: import('./ori_like_platformer/bash_in_action.png'),
     description:
       "Hello! This is my first dev log since I became a game dev. Since it’s the first dev log I’ll give some background on myself.",
   },
